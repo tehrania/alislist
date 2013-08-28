b9877787ec51564f9f1555421dca0ff6139cb845
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-27 22:02:01 -0700

    p2

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,21 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+get '/create_post' do 
+  erb :create_post
+end
+
+post '/category' do 
+ 
+  category_id = params[:post][:category_id] 
+  Post.create(params[:post])
+  redirect to ("/category/#{params[:post][:category_id]}")
+end 
+
+get '/category/:id' do   
+  p params
+  category = Category.find(params[:id])
+  @posts = category.posts 
+  erb :category
 end
--- db/migrate/20130827164042_create_posts.rb
+++ db/migrate/20130827164042_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration
 
   def change
     create_table :posts do |t|
+  
       t.string :title
       t.string :price
       t.string :description
